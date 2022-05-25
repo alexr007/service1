761f8fcfa0a6a74a6fbf5b3acd7eda9e8df2cd2b
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2022-05-25 20:49:56 +0300

    simplified

--- src/main/java/service1/AppController.java
+++ src/main/java/service1/AppController.java
@@ -2,11 +2,9 @@ package service1;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @Log4j2
 @RestController
